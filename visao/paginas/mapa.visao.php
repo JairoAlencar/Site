0ea4f21b8aa667f2357eeabb99c82d916b411ed1
1831216 <1831216@LABCMAQ19.itp.local>
2019-04-23 12:40:45 -0300

    amem

--- visao/paginas/mapa.visao.php
+++ visao/paginas/mapa.visao.php
@@ -1,4 +1,4 @@
 <h1><strong>Como nos encontrar?</strong></h1>
 	<h2>Localize-nos no mapa</h2>
 		<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d914.06999925556!2d-48.019295784422816!3d-23.594289192576987!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spt-BR!2sbr!4v1555334518794!5m2!1spt-BR!2sbr" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
-			<h6>by: thiaguin</h6>
+		<h6>by: thiaguin</h6>
