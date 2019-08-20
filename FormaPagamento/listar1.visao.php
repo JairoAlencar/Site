088f4ea89917834822e1995a1362f3b20750e2b7
SamuelVelosoM <47746454+SamuelVelosoM@users.noreply.github.com>
2019-08-20 11:27:20 -0300

    Add files via upload

---
+++ FormaPagamento/listar1.visao.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html> 
+<html>
+<head>
+  <title></title> 
+  <link rel="stylesheet" type="text/css" href="./publico/css/pagprin">  
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">  
+</head>
+<body>
+
+	<h2>Detalhes da forma de pagamento</h2>
+
+		<p>Id: <?=$FormaPagamento['idFormaPagamento']?> </p>
+		<p>Descrição: <?=$FormaPagamento['descricao']?> </p>
+
+		<a href="./FormaPagamento/listarFormaPagamento">Voltar</a>
+
+</body>
+</html>		
