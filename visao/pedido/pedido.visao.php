e9c48e25bcce94400127484c259aeece723e7c8f
JairoAlencar <47746688+JairoAlencar@users.noreply.github.com>
2019-10-27 20:28:10 -0300

    layout do finalizar pedido, com poucos forms

--- visao/pedido/pedido.visao.php
+++ visao/pedido/pedido.visao.php
@@ -1,3 +1,45 @@
-<?php
-echo "hello world, vai capetinha";
+<div style="clear: left;">
+	<?php
+echo "Ordem de formularios: Endereço usuario, frete(data de entrega e valor), forma de pagamento, cupom. Resumo de tudo isso";
 ?>
+</div>
+
+<div style="float: left; width: 60%;">
+	<div name="end entreg">
+		<h4><i class="fas fa-home"></i> Endereço de entrega</h4>	
+
+	</div>
+
+	<div name="frete">
+		<h4><i class="fas fa-truck"></i> Prazo frete</h4>	
+
+	</div>
+
+	<div name="form pag">
+		<h4><i class="fab fa-amazon-pay"></i> Forma de pagamento</h4>
+
+			<form action="" method="" class="box">
+				
+			</form>
+	</div>
+
+	<div name="cupom">
+		<h4><i class="fas fa-percent"></i> Cupom</h4>
+
+			<form action="" method="" class="box" style="padding: 0px 0px 0px 0px;">
+				<input type="text" name="nome_cupom" placeholder="Infome um cupom valido">
+					<br>
+				<input type="submit" name="vai_cupom" value="Aplicar">	
+			</form>
+	</div>
+</div>
+
+<div class="resumo_fim" style="float: right;">
+	<h4><i class="fas fa-box-open"></i> Resumo</h4>
+
+</div>
+
+
+<div style="clear: left;" class="box">
+	<button><i class="fas fa-box"></i> Finalizar pedido</button>
+</div>
