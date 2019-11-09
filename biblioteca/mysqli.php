b55fa80814e1b04e23a70cf3a4b2bbf30ca1f84f
JairoAlencar <47746688+JairoAlencar@users.noreply.github.com>
2019-11-09 16:59:48 -0300

    09/11 - manipulação de arquivo(teste 1)

--- biblioteca/mysqli.php
+++ biblioteca/mysqli.php
@@ -1,7 +1,21 @@
 <?php
 
 function conn() {
-    $cnx = mysqli_connect("localhost", "root", "", "Loja");
+	$local = "biblioteca/manipulação/local.csv";
+	$servidor = "biblioteca/manipulação/serv.csv";
+
+	$file = fopen($arquivo, 'r');
+		$linha = fgets($file);
+			$conexao = explode(',', $linha);
+				$server = $conexao[0];
+				$usuario = $conexao[1];
+				$senha = $conexao[2];
+				$database = $conexao[3];
+
+	fclose($file);			
+
+	$cnx = mysqli_connect($server, $usuario, $senha, $database);
+    //$cnx = mysqli_connect("localhost", "root", "", "Loja");
 
     if (!$cnx) die('Deu errado a conexao!');
     return $cnx;
